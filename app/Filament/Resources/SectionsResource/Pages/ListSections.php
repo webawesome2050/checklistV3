88b155cbfeacf984f93ae7ebd6e57f9ad073ee70
webawesome2050 <webawesome2050@gmail.com>
2023-10-24 01:50:36 +0530

    site_level_changes

--- app/Filament/Resources/SectionsResource/Pages/ListSections.php
+++ app/Filament/Resources/SectionsResource/Pages/ListSections.php
@@ -6,6 +6,9 @@ use App\Filament\Resources\SectionsResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder; 
+
 class ListSections extends ListRecords
 {
     protected static string $resource = SectionsResource::class;
@@ -17,4 +20,19 @@ class ListSections extends ListRecords
             Actions\CreateAction::make()->label('Create Area'),
         ];
     }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()->icon('heroicon-m-check-badge'),
+                    // ->badge(CheckList::query()->count()),
+            'Site 34' => Tab::make()->icon('heroicon-m-check-circle')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('site_id', 1)),
+                // ->badge(CheckList::query()->where('is_approved', true)->count()),
+            'Site 1263' => Tab::make()->icon('heroicon-m-x-circle')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('site_id', 2))
+                // ->badge(CheckList::query()->where('is_approved', false)->count()),
+        ];
+    }
+
 }
