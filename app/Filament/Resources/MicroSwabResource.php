f3b98f89a4c4e4aa563d80d69ef486a89ec19c6a
webawesome2050 <webawesome2050@gmail.com>
2023-11-16 10:25:07 +0530

    save changes

--- app/Filament/Resources/MicroSwabResource.php
+++ app/Filament/Resources/MicroSwabResource.php
@@ -2,37 +2,30 @@
 
 namespace App\Filament\Resources;
 
-use Filament\Forms;
-use App\Models\Role;
-use Filament\Tables;
-use Filament\Forms\Form;
+use App\Filament\Resources\MicroSwabResource\Pages;
 use App\Models\CheckList;
-use Filament\Tables\Table;
 use App\Models\CheckListItem;
-use App\Models\EntriesMaster;
+use App\Models\Role;
 use App\Models\SubSectionItem;
-use Filament\Resources\Resource;
-use Filament\Forms\Components\Tabs;
-use Filament\Forms\Components\Text;
-use Filament\Tables\Actions\Action;
-use Filament\Forms\Components\Radio;
+use Filament\Forms;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Hidden;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\Wizard;
-use Illuminate\Support\Facades\Route;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\Textarea;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Filament\Forms\Components\CheckboxList;
-use Filament\Forms\Components\DateTimePicker;
-use App\Filament\Resources\MicroSwabResource\Pages;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\MicroSwabResource\RelationManagers;
 
 class MicroSwabResource extends Resource
 {
@@ -41,46 +34,56 @@ class MicroSwabResource extends Resource
     // protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     protected static ?string $navigationGroup = 'Site 1263 Forms';
+
     protected static ?int $navigationSort = 6;
+
     protected static ?string $navigationLabel = 'Micro SPC Swab Check';
+
     protected static ?string $breadcrumb = 'Micro SPC Swab Check';
 
     public static function getNavigationBadge(): ?string
     {
-        return static::getModel()::where('type_id',5)->count();
+        return static::getModel()::where('type_id', 5)->count();
     }
 
     public static function form(Form $form): Form
-    { 
- 
-       
+    {
+
         $optionsValue = [];
         for ($i = 0; $i <= 60; $i++) {
             $optionsValue[$i] = $i;
         }
 
-        $url = request()->url(); 
+        $url = request()->url();
         preg_match('/\/checklists\/(\d+)\/edit/', $url, $matches);
         if (isset($matches[1])) {
             $id = $matches[1];
-            session(['checklist_id' => $id]); 
+            session(['checklist_id' => $id]);
         } else {
             $id = session('checklist_id');
-        } 
+        }
 
-        
-         
         $checklistItems = CheckListItem::where('check_list_id', 1)->get();
-        $checklistItemsBySectionAndSubsection = $checklistItems->groupBy(['section_id', 'sub_section_id']);
+        // $checklistItemsBySectionAndSubsection = $checklistItems->groupBy(['section_id', 'sub_section_id']);
+
+        $checklistItemsBySectionAndSubsection = $checklistItems
+            ->groupBy(['section_id', 'sub_section_id'])
+            ->map(function ($subsectionGroups) {
+                return $subsectionGroups
+                    ->sortBy(function ($items) {
+                        return $items->first()->subSection->sequence_number;
+                    });
+                // ->take(7);
+            });
 
         foreach ($checklistItemsBySectionAndSubsection as $sectionId => $subsectionGroups) {
             $sectionName = $subsectionGroups->first()->first()->section->name;
             $itemCount = count($subsectionGroups);
-           // $sectionName =   $sectionName."-".$itemCount;
-            $sectionName =   $sectionName;
+            // $sectionName =   $sectionName."-".$itemCount;
+            $sectionName = $sectionName;
 
             $sectionComponents = [];
-            $subsectionNameArray  = [];
+            $subsectionNameArray = [];
             foreach ($subsectionGroups as $subsectionId => $checklistItemsInSubsection) {
                 $subsectionName = $checklistItemsInSubsection->first()->first()->subSection->name;
                 // Retrieve sub-section items for the current sub-section
@@ -106,103 +109,108 @@ class MicroSwabResource extends Resource
                 } else {
                     $formFields = [];
                 }
-                
+
                 $matchingItem = $checklistItemsInSubsection->first(function ($item) use ($subsectionId) {
                     return $item->sub_section_id === $subsectionId;
                 });
-                $matchingItem = $checklistItemsInSubsection->first(function ($item) use ($subsectionId) {
-                    return $item->sub_section_id === $subsectionId;
-                });
+                // $matchingItem = $checklistItemsInSubsection->first(function ($item) use ($subsectionId) {
+                //     return $item->sub_section_id === $subsectionId;
+                // });
 
                 if ($matchingItem) {
-                    $subsectionName = $matchingItem->subSection->name;  
+
+                    $isParent = $matchingItem->subSection->is_parent;
+                    $hasParentSubSection = $matchingItem->subSection->parent_sub_section_id !== null;
+                    $sectionClass = 'section-portion cursor-pointer';
+                    // If it has a parent sub-section, change the class
+                    if ($hasParentSubSection) {
+                        $sectionClass = 'alternate-class';
+                    }
+
+                    $subsectionName = $matchingItem->subSection->name;
                     $subsectionSection = Section::make($subsectionName)
-                    ->extraAttributes([
-                        'class' => 'section-portion cursor-pointer'
-                    ])
-                    ->collapsible()
+                        ->extraAttributes([
+                            'class' => $sectionClass,
+                        ])
+                        ->collapsible()
                     // ->description($matchingItem->subSection->atp_frequency ? 'ATP check RLU Frequency => '.$matchingItem->subSection->atp_frequency : '' )
-                    ->columns(4)
-                    ->compact();
-                    
+                        ->columns(4)
+                        ->compact();
+
                 } else {
                     $subsectionSection = Section::make('Section')
-                    ->columns(4)
-                    ->compact()
-                    ->collapsed(); // Set the section to be collapsed by default
+                        ->columns(4)
+                        ->compact()
+                        ->collapsed(); // Set the section to be collapsed by default
                 }
-                if(count($subSectionItems) > 0) {
+                if (count($subSectionItems) > 0) {
                     // \Log::info($formFields);
-                } 
+                }
                 // $formFields = [];
                 $stepFields = [];
-                if(count($subSectionItems) > 0) {
-                    $stepFields[]   =  
+                if (count($subSectionItems) > 0) {
+                    $stepFields[] =
                     Section::make('Select machine number')
-                    ->extraAttributes([
-                        'class' => 'machine-section'
-                    ])
-                    ->icon('heroicon-m-megaphone')
+                        ->extraAttributes([
+                            'class' => 'machine-section',
+                        ])
+                        ->icon('heroicon-m-megaphone')
                     // ->aside()
-                   ->schema([ 
-                    $formFields[] = $itemCode
-                    ]);
+                        ->schema([
+                            $formFields[] = $itemCode,
+                        ]);
                 }
 
                 foreach ($checklistItemsInSubsection as $checklistItem) {
-                   
-                    $description = $checklistItem->m_frequency ? 'M Frequency =>'.$checklistItem->m_frequency : '';
-                    $description.=$checklistItem->c_frequency ? ' ---- C. Frequency =>'.$checklistItem->c_frequency : '';
-                    $description.=$checklistItem->a_frequency ? ' ---- A. Frequency =>'.$checklistItem->a_frequency : '';
 
-                    $stepFields[]   =  
+                    $description = $checklistItem->m_frequency ? 'M Frequency =>'.$checklistItem->m_frequency : '';
+                    $description .= $checklistItem->c_frequency ? ' ---- C. Frequency =>'.$checklistItem->c_frequency : '';
+                    $description .= $checklistItem->a_frequency ? ' ---- A. Frequency =>'.$checklistItem->a_frequency : '';
+
+                    $stepFields[] =
                  Section::make($checklistItem->name)
-                 ->description($description)
-                 ->aside()
-                ->schema([ 
-                        $formFields[] =  Textinput::make("micro_SPC_swab_{$checklistItem->id}")
-                        // $formFields[] =  Select::make("micro_SPC_swab_{$checklistItem->id}")
-                        ->label('Micro SPC swab Check'),
-                        $formFields[] = Textarea::make("comments_corrective_actions_$checklistItem->id")->label('Comments & Corrective Actions')->name('comments_corrective_actions')
-                        ->rows(1),  
-                        Radio::make("action_taken_$checklistItem->id")->label('Is Testing Done')
-                        ->options([
-                            'Yes' => 'Yes',
-                            'No' => 'No'
-                        ]), 
-                        $formFields[] =  Hidden::make("entry_id_$checklistItem->id"),
-                        
-                    ])->columns(3)->compact(); 
-                } 
-                $subsectionSection->schema($stepFields); 
-                $sectionComponents[] = $subsectionSection; 
+                     ->description($description)
+                     ->aside()
+                     ->schema([
+                         $formFields[] = Textinput::make("micro_SPC_swab_{$checklistItem->id}")
+                         // $formFields[] =  Select::make("micro_SPC_swab_{$checklistItem->id}")
+                             ->label('Micro SPC swab Check'),
+                         $formFields[] = Textarea::make("comments_corrective_actions_$checklistItem->id")->label('Comments & Corrective Actions')->name('comments_corrective_actions')
+                             ->rows(1),
+                         Radio::make("action_taken_$checklistItem->id")->label('Is Testing Done')
+                             ->options([
+                                 'Yes' => 'Yes',
+                                 'No' => 'No',
+                             ]),
+                         $formFields[] = Hidden::make("entry_id_$checklistItem->id"),
+
+                     ])->columns(3)->compact();
+                }
+                $subsectionSection->schema($stepFields);
+                $sectionComponents[] = $subsectionSection;
             }
             $sectionStep = Tab::make($sectionName)->schema($sectionComponents);
             $wizardSteps[] = $sectionStep;
         }
-        
-
-        
- 
 
         return $form
             ->schema([
                 Forms\Components\Group::make()
                     ->schema([
-                        Tabs::make('Label')->tabs($wizardSteps)
+                        Tabs::make('Label')->tabs($wizardSteps),
                     ])
-                    ->columnSpan(['lg' =>2]),
+                    ->columnSpan(['lg' => 2]),
 
                 Forms\Components\Section::make()
                     ->schema([
                         Hidden::make('id'),
                         Forms\Components\TextInput::make('person_name')
-                        ->label('Person Name')
-                        ->maxLength(255)
-                        ->required(), 
+                            ->label('Person Name')
+                            ->maxLength(255)
+                            ->required(),
                         DatePicker::make('entry_detail')
-                        ->label('Entry Detail')
-                        ->native(false),
+                            ->label('Entry Detail')
+                            ->native(false),
                         // DateTimePicker::make('next_inspection_detail')
                         // ->label('Next Inspectin Date')
                         // ->native(false),
@@ -213,29 +221,28 @@ class MicroSwabResource extends Resource
     }
 
     public static function table(Table $table): Table
-    { 
+    {
         return $table
             ->columns([
                 //
                 // TextColumn::make('id'),
                 TextColumn::make('name'),
                 TextColumn::make('created_at')
-                ->dateTime()
-                ->label('Created on'),
+                    ->dateTime()
+                    ->label('Created on'),
                 Tables\Columns\IconColumn::make('is_approved')
                     ->boolean(),
                 TextColumn::make('')
-                ->label('Status')
-                ->description(function (CheckList $record) {
-                    $res= $record->is_approved == 0 ;
-                    if ($res){
-                        return "Pending";
-                    }
-                    else{
-                        return "Approved";
-                    }
-                }),
-                TextColumn::make('site.name')
+                    ->label('Status')
+                    ->description(function (CheckList $record) {
+                        $res = $record->is_approved == 0;
+                        if ($res) {
+                            return 'Pending';
+                        } else {
+                            return 'Approved';
+                        }
+                    }),
+                TextColumn::make('site.name'),
             ])
             ->striped()
             ->filters([
@@ -243,35 +250,35 @@ class MicroSwabResource extends Resource
             ->actions([
 
                 Action::make('Download Report')->label('Download Report')
-                ->url(fn (CheckList $record): string => route('generate.micro', $record))
-                ->openUrlInNewTab()
-                ->visible(function (CheckList $record): bool {
-                    return ($record->is_approved && auth()->user()->hasRole(Role::ROLES['approver'])) || ($record->is_approved && auth()->user()->hasRole(Role::ROLES['admin']));
-                })
-                ->icon('heroicon-m-arrow-down-on-square'),
+                    ->url(fn (CheckList $record): string => route('generate.micro', $record))
+                    ->openUrlInNewTab()
+                    ->visible(function (CheckList $record): bool {
+                        return ($record->is_approved && auth()->user()->hasRole(Role::ROLES['approver'])) || ($record->is_approved && auth()->user()->hasRole(Role::ROLES['admin']));
+                    })
+                    ->icon('heroicon-m-arrow-down-on-square'),
                 Tables\Actions\ViewAction::make()->label('View and Approve')
-                ->visible(function (CheckList $record): bool {
-                    return (!$record->is_approved && auth()->user()->hasRole(Role::ROLES['approver'])) || (!$record->is_approved && auth()->user()->hasRole(Role::ROLES['admin']));
-                }),
+                    ->visible(function (CheckList $record): bool {
+                        return (! $record->is_approved && auth()->user()->hasRole(Role::ROLES['approver'])) || (! $record->is_approved && auth()->user()->hasRole(Role::ROLES['admin']));
+                    }),
                 Tables\Actions\EditAction::make()
-                ->visible(function (CheckList $record): bool {
-                    return (!$record->is_approved && auth()->user()->hasRole(Role::ROLES['approver'])) || (!$record->is_approved && auth()->user()->hasRole(Role::ROLES['admin']));
-                }),
+                    ->visible(function (CheckList $record): bool {
+                        return (! $record->is_approved && auth()->user()->hasRole(Role::ROLES['approver'])) || (! $record->is_approved && auth()->user()->hasRole(Role::ROLES['admin']));
+                    }),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]
-        )
-        ->modifyQueryUsing(fn (Builder $query) => $query->where('type_id', 5));
+            )
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('type_id', 5));
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -280,5 +287,5 @@ class MicroSwabResource extends Resource
             'view' => Pages\ViewMicroSwab::route('/{record}'),
             'edit' => Pages\EditMicroSwab::route('/{record}/edit'),
         ];
-    }    
+    }
 }
