c8c05ec8ba48d82494d3dd3b853026a88419a305
webawesome2050 <webawesome2050@gmail.com>
2023-12-15 07:17:04 +0530

    changes in the gmp

--- app/Models/FormTvSite2.php
+++ app/Models/FormTvSite2.php
@@ -9,10 +9,8 @@ class FormTvSite2 extends Model
 {
     use HasFactory;
 
-
     protected $table = 'form_tv_site2';
 
- 
     protected $fillable = [
         'id',
         'name',
@@ -26,6 +24,7 @@ class FormTvSite2 extends Model
         'base_area_f1',
         'base_area_f2',
         'base_area_cool_room',
+        'base_area_raw_chiller',
         'medium_area_cool_freezer',
         'medium_area_cool_chiller1',
         'medium_area_cool_chiller2',
@@ -37,11 +36,11 @@ class FormTvSite2 extends Model
         'factory_lunch_room_fridge',
         'office_staff_lunch_room_fridge',
         'is_verified',
-        'verified_by'
-     ];
+        'verified_by',
+    ];
 
-     public function tv_lines()
-     {
-         return $this->hasMany(FormTvLineSite2::class);
-     }
+    public function tv_lines()
+    {
+        return $this->hasMany(FormTvLineSite2::class);
+    }
 }
